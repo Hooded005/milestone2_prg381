4636c8627bf29a0dfb184d409cec465758773d98
Sean Botha <seanbotha2003@gmail.com>
2024-08-21 10:11:15 +0200

    Test

---
+++ src/config/javaDB-config.sql
@@ -0,0 +1,9 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  seanb
+ * Created: 21 Aug 2024
+ */
+
